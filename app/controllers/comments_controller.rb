c364154347c55ee8f59b724bb538880b0ca57c8c
Amber Sharma <ambshar@gmail.com>
2014-01-31 13:51:25 -0800

    user comment assoc

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,17 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    
     @comment = @post.comments.build(comment_params)
+    @comment.user_id = session[:user_id]
+    
     
     if @comment.save
       flash[:notice]= "Your comment was added"
+
       redirect_to post_path(@post)
     else
+      
       render 'posts/show'
     end
 
@@ -39,6 +44,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :user_id)
   end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You are logged in"
+      
+      redirect_to root_path
+
+    else
+      flash[:notice] = "There is something wrong with your username or password"
+      redirect_to login_path
+
+    end
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've logged out"
+    redirect_to root_path
+  end
+
+end
