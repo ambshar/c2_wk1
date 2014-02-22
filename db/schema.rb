7183b6dfe207a6c982f6feff9e0ba48744e53eb7
Amber Sharma <ambshar@gmail.com>
2014-02-21 16:41:04 -0800

    add user_id integer type to votes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222003323) do
+ActiveRecord::Schema.define(version: 20140222003908) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140222003323) do
     t.integer  "voteable_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
